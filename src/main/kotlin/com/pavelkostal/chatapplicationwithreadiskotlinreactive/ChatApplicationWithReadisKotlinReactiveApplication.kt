7b6b19d46f6417a531915c6240dfb52cf451b64e
Katzz Win11 PC <katzz@seznam.cz>
2024-05-06 20:57:54 +0200

    initial commit

---
+++ src/main/kotlin/com/pavelkostal/chatapplicationwithreadiskotlinreactive/ChatApplicationWithReadisKotlinReactiveApplication.kt
@@ -0,0 +1,11 @@
+package com.pavelkostal.chatapplicationwithreadiskotlinreactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatApplicationWithReadisKotlinReactiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatApplicationWithReadisKotlinReactiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/pavelkostal/chatapplicationwithreadiskotlinreactive/ChatApplicationWithReadisKotlinReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pavelkostal.chatapplicationwithreadiskotlinreactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatApplicationWithReadisKotlinReactiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

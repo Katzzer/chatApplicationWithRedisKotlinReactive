08cc432a270e0354324e76111de97691cba3fdbb
pavelkostal <pavelkostal@ders.cz>
2024-05-16 15:06:18 +0200

    Refine UserRepository functions for user management
    Refined a number of methods in UserRepository like delete, deleteAll, deleteById, deleteAllById, and save, providing actual functionalities that previously didn't exist. Shifted userList to a mutableList to support addition and removal of users.

--- src/main/kotlin/com/pavelkostal/chatapplicationwithreadiskotlinreactive/repository/FakeUserRepository.kt
+++ src/main/kotlin/com/pavelkostal/chatapplicationwithreadiskotlinreactive/repository/FakeUserRepository.kt
@@ -14,7 +14,7 @@ class FakeUserRepository : ReactiveCrudRepository<User, String> {
 
     private val john = User("1", "john", encoder.encode("john"), setOf("ROLE_USER"))
     private val jack = User("2", "jack", encoder.encode("jack"), setOf("ROLE_USER"))
-    private val userList = listOf(john, jack)
+    private var userList = mutableListOf(john, jack)
 
     fun findByUsername(username: String): Mono<User> = Mono.justOrEmpty(userList.find { it.username == username })
 
@@ -22,9 +22,15 @@ class FakeUserRepository : ReactiveCrudRepository<User, String> {
 
     override fun findAll(): Flux<User> = Flux.fromIterable(userList)
 
-    override fun deleteById(id: String): Mono<Void> = Mono.empty()
+    override fun deleteById(id: String): Mono<Void> {
+        userList.removeIf { it.getId() == id}
+        return Mono.empty()
+    }
 
-    override fun deleteAll(entities: Iterable<User>): Mono<Void> = Mono.empty()
+    override fun deleteAll(entities: Iterable<User>): Mono<Void> {
+        entities.forEach { entity -> userList.removeIf { it.getId() == entity.getId() } }
+        return Mono.empty()
+    }
 
     override fun existsById(id: String): Mono<Boolean> = Mono.just(userList.any { it.getId() == id })
 
@@ -32,27 +38,41 @@ class FakeUserRepository : ReactiveCrudRepository<User, String> {
 
     override fun findAllById(ids: Iterable<String>): Flux<User> = Flux.fromIterable(userList.filter { ids.contains(it.getId()) })
 
-    override fun delete(entity: User): Mono<Void> = Mono.empty() // Does nothing for now
+    override fun delete(entity: User): Mono<Void> {
+        userList.removeIf { it.getId() == entity.getId()}
+        return Mono.empty()
+    }
 
-    override fun deleteAll(): Mono<Void> = Mono.empty() // Does nothing for now
+    override fun deleteAll(): Mono<Void> {
+        userList.clear()
+        return Mono.empty()
+    }
 
-    override fun deleteById(id: Publisher<String>): Mono<Void> = Mono.empty() // Does nothing for now
+    override fun deleteById(id: Publisher<String>): Mono<Void> {
+        return Mono.from(id).doOnNext { idStr -> userList.removeIf { it.getId() == idStr } }.then()
+    }
 
-    override fun deleteAllById(ids: Iterable<String>): Mono<Void> = Mono.empty() // Does nothing for now
+    override fun deleteAllById(ids: Iterable<String>): Mono<Void> {
+        ids.forEach { id -> userList.removeIf { it.getId() == id } }
+        return Mono.empty()
+    }
 
-    override fun deleteAll(entityStream: Publisher<out User>): Mono<Void> = Mono.empty() // Does nothing for now
+    override fun deleteAll(entityStream: Publisher<out User>): Mono<Void> = throw RuntimeException("Not implemented")  // Does nothing for now
 
-    override fun findById(id: Publisher<String>): Mono<User> = Mono.empty() // Does nothing for now
+    override fun findById(id: Publisher<String>): Mono<User> {
+        return Mono.from(id).flatMap { idStr -> Mono.justOrEmpty(userList.find { it.getId() == idStr }) }
+    }
 
-    override fun existsById(id: Publisher<String>): Mono<Boolean> = Mono.empty() // Does nothing for now
+    override fun existsById(id: Publisher<String>): Mono<Boolean> = throw RuntimeException("Not implemented")  // Does nothing for now
 
-    override fun findAllById(idStream: Publisher<String>): Flux<User> = Flux.empty() // Does nothing for now
+    override fun findAllById(idStream: Publisher<String>): Flux<User> = throw RuntimeException("Not implemented")  // Does nothing for now
 
     override fun <S : User?> save(entity: S & Any): Mono<S> {
+       userList.add(entity)
        return Mono.empty()
     }
 
-    override fun <S : User?> saveAll(entities: Iterable<S>): Flux<S> = Flux.empty() // Does nothing for now
+    override fun <S : User?> saveAll(entities: Iterable<S>): Flux<S> = throw RuntimeException("Not implemented")  // Does nothing for now
 
-    override fun <S : User?> saveAll(entityStream: Publisher<S>): Flux<S> = Flux.empty() // Does nothing for now
+    override fun <S : User?> saveAll(entityStream: Publisher<S>): Flux<S> = throw RuntimeException("Not implemented") // Does nothing for now
 }
